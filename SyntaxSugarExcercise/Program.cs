aa12039f47adc34df91c781b5a8d0c0d3545ee4b
Jose <josepls.v@hotmail.com>
2022-03-23 10:46:46 -0700

    first commit

---
+++ SyntaxSugarExcercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SyntaxSugarExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Extra code added for user input instead of just 4 as variable
+            Console.WriteLine("Give me a number.");
+
+            //Excercise 1 has shortened from 11 lines of code to 3
+            var answer = Convert.ToInt32(Console.ReadLine());
+            var response = (answer < 4) ? " is less than nine" : " is greater than or equal to nine";
+            Console.WriteLine($"If you're answer is {answer}, it{response}");
+        }
+    }
+}
